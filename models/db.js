f5e2e5d086b74bffc300e6542b07bd47ef4c08b9
Muhammad <muhammadabdrabou46@gmail.com>
2026-05-18 18:31:17 +0300

    connect feedback and database

--- models/db.js
+++ models/db.js
@@ -14,10 +14,14 @@ const db = {
     filename: path.join(__dirname, "../data/collections.db"),
     autoload: true,
   }),
+  feedbacks: Datastore.create({
+    filename: path.join(__dirname, "../data/feedbacks.db"),
+    autoload: true,
+  }),
 };
 
 // Indexes
 db.users.ensureIndex({ fieldName: "email", unique: true });
 db.todos.ensureIndex({ fieldName: "userId" });
-
+db.feedbacks.ensureIndex({ fieldName: "userId" });
 module.exports = db;
